d6e89579e8c987071e3e9a8d98fdfa885669016a
AntonioLoureiro <ajose.loureiro@gmail.com>
2020-03-18 19:14:40 +0000

    Update Dict.jl

--- src/ECharts/Dict.jl
+++ src/ECharts/Dict.jl
@@ -58,14 +58,6 @@ function dict(series::Series)
     
 end
 
-function dict_any(d::Dict)
-    d=convert(Dict{String,Any},d)
-    for (k,v) in d
-        v isa Dict ? d[k]=dict_any(v) : nothing
-    end
-    return d
-end
-
 function dict(p::Plot)
     
     options=p.options
@@ -89,6 +81,6 @@ function dict(p::Plot)
         
     merge!(options,series_d)
     
-    return dict_any(options)
+    return options
 end
 
